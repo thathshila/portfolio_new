b884d5d9d99b872600f915b1f331545694a7d6a7
Thathshila <thathshilaashanganie9501@gmail.com>
2024-10-26 11:09:37 +0530

    first commit

---
+++ asserts/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text",{
+    strings:["Frontend Developer","Backend Developer","A Passionate Software_Engineer"],
+    typeSpeed:80,
+    backSpeed:80,
+    backDelay:100,
+    loop: true
+})
